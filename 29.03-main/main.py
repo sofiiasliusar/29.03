bfd4f378d3cd3d311dba5484471a52c0f8cc1f71
Sofiia Sliusar <144224845+sofiiasliusar@users.noreply.github.com>
2024-04-05 11:29:30 +0300

    Add files via upload

--- 29.03-main/main.py
+++ 29.03-main/main.py
@@ -10,6 +10,7 @@ from kivy.utils import platform
 from kivy.uix.image import Image
 from kivy.properties import NumericProperty
 from kivy.animation import Animation
+from random import randint
 # 2
 class MenuScreen(Screen):
     def __init__(self, **kwargs):
@@ -20,6 +21,8 @@ class GameScreen(Screen):
     points = NumericProperty(0)
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+    def on_enter(self, *args):
+        self.ids.donut.new_donut()
 
 class Donut(Image):
     is_anim = False
@@ -40,9 +43,17 @@ class Donut(Image):
             anim = Animation(x=x-5, y = y-5, duration = 0.05) + \
                 Animation(x=x, y = y, duration = 0.05)
             anim.start(self)
+            self.is_anim = True
+            anim.on_complete = lambda *args: setattr(self, 'is_anim', False)
         return super().on_touch_down(touch)
-    
+    def new_donut(self):
+        self.donut = app.LEVELS[randint(0, len(app.LEVELS))-1] #index 8 number 9
+        self.source = app.DONUT[self.donut]['source'] #CONSTANTS
+        self.source = app.DONUT[self.donut]['hp']
+        
 class MainApp(App):
+    #LEVELS
+    #PLANETS
     def build(self):
         sm = ScreenManager()
         sm.add_widget(MenuScreen(name = 'menu'))
@@ -53,4 +64,5 @@ class MainApp(App):
         Window.size = (400,800)
         Window.left = 750
         Window.top = 100
-MainApp().run()
+app = MainApp
+app().run()
